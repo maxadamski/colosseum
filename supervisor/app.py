e821c2ff18c4ce736541781d453d34af7b4d2fa2
Slawomir Gilewski <gilewski.slawomir@gmail.com>
2020-12-07 14:24:30 +0100

    Added file handling.

--- supervisor/app.py
+++ supervisor/app.py
@@ -10,6 +10,8 @@ from passlib.hash import sha256_crypt
 import lxc
 import os, stat, shutil
 
+from files import *
+
 config = toml.load('config.toml')
 
 lxc_dir = config['lxc_dir']
@@ -22,18 +24,21 @@ if config['force_https']:
     app.add_middleware(HTTPSRedirectMiddleware)
 
 app.add_middleware(CORSMiddleware,
-    allow_origins=config['allow_origins'],
-    allow_methods=['*'],
-    allow_headers=['*'])
+                   allow_origins=config['allow_origins'],
+                   allow_methods=['*'],
+                   allow_headers=['*'])
+
 
 async def api_token(api_token: str):
     if not sha256_crypt.verify(api_token, config['api_token']):
         raise HTTPException(400, 'Invalid token')
 
+
 @app.get('/job/{id}')
 async def get_job(id: int):
     # TODO: get results of job from RAM/redis
     return dict(status='?', description='?')
+
 
 @app.put('/job/{id}')
 async def new_job(id: int, game_id: int, p1_id: int, p2_id: int):
@@ -41,10 +46,14 @@ async def new_job(id: int, game_id: int, p1_id: int, p2_id: int):
     # TODO: store results in RAM/redis
     return
 
+
 @app.put('/player/{id}')
 async def new_player(id: int, env_id: int, data: UploadFile = File(...), automake: bool = True):
-    # TODO: store player files, compile if needed
-    
+    submission_dir = get_submission_directory(id, init=True)
+    clear_dir_contents(submission_dir)
+    save_and_unzip_files(submission_dir, data, 'player')
+    # TODO: compile if needed
+
     # FIXME: containers should be in new_job
     c = lxc.Container(str(id))
     player = f'/tmp/{data.filename}'
@@ -53,14 +62,28 @@ async def new_player(id: int, env_id: int, data: UploadFile = File(...), automak
     f.close()
 
     cwd = os.getcwd()
-    c.create('player', 0, {'colosseum':cwd, 'player':player})
+    c.create('player', 0, {'colosseum': cwd, 'player': player})
 
     c.start(useinit=True, daemonize=False, cmd=('/util/make_player', str(automake)))
-    #c.start(useinit=True, daemonize=False, cmd=('bash',))
+    # c.start(useinit=True, daemonize=False, cmd=('bash',))
     return
+
+
+@app.put('/ref_player/{id}')
+async def new_ref_player(id: int, game_id: int, env_id: int, data: UploadFile = File(...)):
+    submission_dir = get_game_submission_directory(game_id, id, init=True)
+    clear_dir_contents(submission_dir)
+    save_and_unzip_files(submission_dir, data, 'player')
+    # TODO: compile if needed
+
+    return
+
 
 @app.put('/game/{id}')
-async def new_game(id: int, env_id: int, data: bytes = File(...), automake: bool = True):
-    # TODO: store judge files, compile if needed
-    return
+async def new_game(id: int, env_id: int, data: UploadFile = File(...)):
+    game_dir, game_files_dir = get_game_directories(id, init=True)
+    clear_dir_contents(game_files_dir)
+    save_and_unzip_files(game_files_dir, data, 'judge')
+    # TODO: compile if needed
 
+    return
---
+++ supervisor/files.py
@@ -0,0 +1,54 @@
+import os
+from pathlib import Path
+import zipfile
+import shutil
+
+FILES_DIR = os.path.join(os.getcwd(), 'files')
+GAMES_DIR = os.path.join(FILES_DIR, 'games')
+SUBMISSIONS_DIR = os.path.join(FILES_DIR, 'submissions')
+
+
+def unpack_temp_zip_file(file_path, destination_path):
+    with zipfile.ZipFile(file_path, 'r') as zip_ref:
+        zip_ref.extractall(destination_path)
+    os.unlink(file_path)
+
+
+def clear_dir_contents(directory):
+    for root, dirs, files in os.walk(directory):
+        for f in files:
+            os.unlink(os.path.join(root, f))
+        for d in dirs:
+            shutil.rmtree(os.path.join(root, d))
+
+
+def get_game_directories(game_id, init=False):
+    game_dir = os.path.join(GAMES_DIR, str(game_id))
+    game_files_dir = os.path.join(game_dir, 'judge')
+    if init:
+        Path(game_files_dir).mkdir(parents=True, exist_ok=True)
+    return game_dir, game_files_dir
+
+
+def get_game_submission_directory(game_id, submission_id, init=False):
+    game_submission_dir = os.path.join(GAMES_DIR, str(game_id), 'submissions', str(submission_id))
+    if init:
+        Path(game_submission_dir).mkdir(parents=True, exist_ok=True)
+    return game_submission_dir
+
+
+def get_submission_directory(submission_id, init=False):
+    submission_dir = os.path.join(SUBMISSIONS_DIR, str(submission_id))
+    if init:
+        Path(submission_dir).mkdir(parents=True, exist_ok=True)
+    return submission_dir
+
+
+def save_and_unzip_files(executable_dir, file, name):
+    ext = os.path.splitext(file.filename)[1]
+    file_path = os.path.join(executable_dir, name + ext)
+    with open(file_path, 'wb+') as f:
+        f.write(file.file.read())
+        f.close()
+    if ext == '.zip':
+        unpack_temp_zip_file(file_path, executable_dir)
