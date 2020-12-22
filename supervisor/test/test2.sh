24e02be23e298ca0d5f38502799b21ec5ab15db0
Max Adamski <maxadamski98@gmail.com>
2020-12-22 09:53:19 +0100

    add game ref support

--- supervisor/test/test2.sh
+++ supervisor/test/test2.sh
@@ -9,5 +9,9 @@ curl -X PUT "http://localhost:8001/player/2" -F env_id=0 -F automake=false -F da
 echo
 curl -X PUT "http://localhost:8001/game/1" -F env_id=0 -F data=@judge.zip
 echo
+curl -X PUT "http://localhost:8001/ref_player/1" -F game_id=1 -F env_id=0 -F data=@player.zip
+echo
+curl -X PUT "http://localhost:8001/job/1?game_id=1&p1_id=1&p2_id=1&is_ref=true"
+echo
 curl -X PUT "http://localhost:8001/job/1?game_id=1&p1_id=1&p2_id=2"
 echo
