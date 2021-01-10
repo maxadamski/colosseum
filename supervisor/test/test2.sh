7b9a965abd8dae488e302fb028acacf75ff0ded3
Max Adamski <maxadamski98@gmail.com>
2021-01-10 21:02:45 +0100

    more tests

--- supervisor/test/test2.sh
+++ supervisor/test/test2.sh
@@ -10,14 +10,18 @@ curl -X PUT "http://localhost:8001/game/1" -F env_id=0 -F data=@judge.zip
 echo
 curl -X PUT "http://localhost:8001/ref_player/1" -F game_id=1 -F env_id=0 -F data=@player1.zip
 echo
+curl -X PUT "http://localhost:8001/ref_player/2" -F game_id=1 -F env_id=0 -F data=@player1.zip
+echo
 curl -X PUT "http://localhost:8001/player/1" -F env_id=0 -F automake=false -F data=@player1.zip
 echo
-curl -X PUT "http://localhost:8001/player/2" -F env_id=0 -F automake=false -F data=@player2.zip
+curl -X PUT "http://localhost:8001/player/2" -F env_id=0 -F automake=false -F data=@player1.zip
 echo
-curl -X PUT "http://localhost:8001/player/3" -F env_id=0 -F automake=false -F data=@player3.zip
+curl -X PUT "http://localhost:8001/player/3" -F env_id=0 -F automake=false -F data=@player1.zip
 echo
 curl -X PUT "http://localhost:8001/job/1001?game_id=1&p1_id=1&p2_id=1&is_ref=true"
 echo
-curl -X PUT "http://localhost:8001/job/1002?game_id=1&p1_id=1&p2_id=2"
+curl -X PUT "http://localhost:8001/job/1002?game_id=1&p1_id=1&p2_id=1"
+echo
+curl -X PUT "http://localhost:8001/job/1003?game_id=1&p1_id=1&p2_id=2"
 echo
 
