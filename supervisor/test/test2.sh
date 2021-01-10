2731ce20d8ccf264eb3594356060290b7fbdda4d
Max Adamski <maxadamski98@gmail.com>
2021-01-10 19:00:22 +0100

    better job_id & game_id tests

--- supervisor/test/test2.sh
+++ supervisor/test/test2.sh
@@ -16,8 +16,8 @@ curl -X PUT "http://localhost:8001/player/2" -F env_id=0 -F automake=false -F da
 echo
 curl -X PUT "http://localhost:8001/player/3" -F env_id=0 -F automake=false -F data=@player3.zip
 echo
-curl -X PUT "http://localhost:8001/job/1?game_id=1&p1_id=1&p2_id=1&is_ref=true"
+curl -X PUT "http://localhost:8001/job/1001?game_id=1&p1_id=1&p2_id=1&is_ref=true"
 echo
-curl -X PUT "http://localhost:8001/job/1?game_id=1&p1_id=1&p2_id=2"
+curl -X PUT "http://localhost:8001/job/1002?game_id=1&p1_id=1&p2_id=2"
 echo
 
